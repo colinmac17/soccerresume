089df3108131f6e2933e7cffdcb7a793453e0d92
Colin McAtee <colinmac@umich.edu>
2017-09-27 18:41:18 -0500

    messed with router

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import About from './components/About';
+import Contact from './components/Contact';
+import NotFound from './components/NotFound';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
+const Root = () => {
+    return (
+    <BrowserRouter>
+    <div>
+        <Route path="/" component={App}/>
+        <Route path="/about" component={About}/>
+        <Route exactly path="/Contact" component={Contact}/>
+    </div>
+    </BrowserRouter>
+    )
+}
+
+render(<Root />, document.getElementById('root'));
 registerServiceWorker();
