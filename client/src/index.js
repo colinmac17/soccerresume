b476503d13afc79716c49e1c3f4c5dcd7b5abee3
Colin McAtee <colinmac@umich.edu>
2017-09-27 19:57:11 -0500

    updated router

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Link, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Link, Route} from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import About from './components/About';
@@ -14,9 +14,11 @@ const Root = () => {
     <div>
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
         <Route exact path="/" component={App}/>
-        <Route path="/about" component={About}/>
-        <Route exactly path="/Contact" component={Contact}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/Contact" component={Contact}/>
+        <Route component={NotFound}/>
     </div>
     </BrowserRouter>
     )
