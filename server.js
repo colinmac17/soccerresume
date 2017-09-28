8a74c9c515b8f98f1c07837256f41267bba66481
Colin McAtee <colinmac@umich.edu>
2017-09-27 20:17:55 -0500

    added in production code to server.js

--- server.js
+++ server.js
@@ -26,6 +26,17 @@ require('./config/passportLocal')(passport, db.user, db.user_settings);
 var routes = require('./controllers/appcontroller');
 app.use('/', routes);
 
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+  
+  // Send every request to the React app
+  // Define any API routes before this runs
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+
 // Syncing our sequelize models and then starting our Express app
 db.sequelize.sync().then(function() {
     app.listen(PORT, function(){
