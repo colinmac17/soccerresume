4f7902499c2900fcd9e7da80851a246b1bf6216d
Colin McAtee <colinmac@umich.edu>
2017-10-04 21:49:26 -0500

    deleted build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var db = require('./models');
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("./client/build"));
+    app.use(express.static(path.join(__dirname, 'build')));
   }
 
 //Sets up the Express app to handle data parsing
@@ -37,7 +37,7 @@ app.use(appRoutes);
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-    const index = path.join(__dirname, './client/build', 'index.html');
+    const index = path.join(__dirname, 'build', 'index.html');
     res.sendFile(index);
 });
 
